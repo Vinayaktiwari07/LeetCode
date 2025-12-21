9fb3cd9d53cc4f1714c7016712dff69f8aded951
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2025-12-21 11:20:10 +0530

    Time: 1 ms (12.87%), Space: 27.1 MB (52.4%) - LeetHub

---
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            bool good = true;
+
+            if (i - k >= 0) {
+                if (nums[i] <= nums[i - k]) {
+                    good = false;
+                }
+            }
+
+            if (i + k < n) {
+                if (nums[i] <= nums[i + k]) {
+                    good = false;
+                }
+            }
+
+            if (good) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+};

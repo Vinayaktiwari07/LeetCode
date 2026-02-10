ed4a79c58e36ea9f077ec26303f42fb544bf69bc
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-02-10 23:57:41 +0530

    Time: 11 ms (5.03%), Space: 18 MB (5.03%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
+        string ans;
+        int i=0;
+        while(true){
+            char curr_ch = 0;
+            for(auto str : strs){
+                if(i >= str.size()){
+                    // out of bound
+                    curr_ch = 0;
+                    break;
+                }
 
-        if (strs.empty()) return "";
-
-        string prefix = "";
-        string first = strs[0];
-
-        for (int j = 0; j < first.size(); j++) {
-            char ch = first[j];
-
-            for (int i = 1; i < n; i++) {
-                if (j >= strs[i].size() || strs[i][j] != ch) {
-                    return prefix;
+                // just started
+                if(curr_ch == 0){
+                    curr_ch = str[i];
+                }
+                else if(str[i] != curr_ch){
+                    curr_ch = 0;
+                    break;
                 }
             }
-
-            prefix += ch;
+            if(curr_ch == 0){
+                break;
+            }
+            ans.push_back(curr_ch);
+            i++;
         }
+        return ans;
 
-        return prefix;
+        // int n = strs.size();
+
+        // if (strs.empty()) return "";
+
+        // string prefix = "";
+        // string first = strs[0];
+
+        // for (int j = 0; j < first.size(); j++) {
+        //     char ch = first[j];
+
+        //     for (int i = 1; i < n; i++) {
+        //         if (j >= strs[i].size() || strs[i][j] != ch) {
+        //             return prefix;
+        //         }
+        //     }
+
+        //     prefix += ch;
+        // }
+
+        // return prefix;
     }
 };

82b3fe06171fd0e99850db0712a46690fe1e6eb4
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-06-01 23:57:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (60%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        int sum = 0;
-        string str = to_string(n);
-        for(int i=0; i<str.size(); i++){
-            int temp = str[i] - '0';
-            sum += temp;
-        }
+        // int sum = 0;
+        // string str = to_string(n);
+        // for(int i=0; i<str.size(); i++){
+        //     int temp = str[i] - '0';
+        //     sum += temp;
+        // }
 
+        // return sum;
+
+        n = abs(n);
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
         return sum;
         
     }

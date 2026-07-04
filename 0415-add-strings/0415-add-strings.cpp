bda2bfa56aae5d9fad0993253aa10ccc6ab46b39
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-07-04 22:51:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (39.54%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    string addRE(string num1, int p1, string num2, int p2, int carry = 0){
+    void addRE(string &num1, int p1, string &num2, int p2, int carry, string &ans){
         //base case
         if(p1 < 0 && p2 < 0){
             if(carry != 0){
-                return string(1, carry + '0');
+                ans.push_back(carry + '0');
             }
-            return "";
+            return;
         }
 
         // ek case solve
@@ -15,17 +15,16 @@ public:
         int csum = n1 + n2 + carry;
         int digit = csum % 10;
         carry = csum / 10;
-        string ans = "";
         ans.push_back(digit + '0');
 
 
         // RE
-        ans += addRE(num1, p1-1, num2, p2-1, carry);
-        return ans;
+        addRE(num1, p1-1, num2, p2-1, carry, ans);
     }
     
     string addStrings(string num1, string num2) {
-        string ans = addRE(num1, num1.size()-1, num2, num2.size()-1);
+        string ans = "";
+        addRE(num1, num1.size()-1, num2, num2.size()-1, 0, ans);
         reverse(ans.begin(), ans.end());
         return ans;
     }

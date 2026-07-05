f89c72827a33e08838739740f8cfa7c61840c3b7
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-07-05 10:44:39 +0530

    Time: 2 ms (42%), Space: 12.8 MB (5.22%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int n = part.length();
-        int pos = s.find(part);
+    void removeOccRE(string &s, string &part){
+        int found = s.find(part);
+        if(found != string::npos){
+            // part string has been located.
+            // please remove it.
+            string left_part = s.substr(0, found);
+            string right_part = s.substr(found+part.size(), s.size());
+            s = left_part + right_part;
 
-        while(pos != string :: npos){
-            s.erase(pos, n);
-            pos = s.find(part);
+            removeOccRE(s, part);
         }
+        else{
+            //base case
+            // all the occurences of the part has been removed from s.
+            return;
+        }
+    }
 
+    string removeOccurrences(string s, string part) {
+        removeOccRE(s, part);
         return s;
-
     }
 };

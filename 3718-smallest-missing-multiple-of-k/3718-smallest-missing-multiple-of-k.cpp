a874a61487b1ca0978fbaf20269aab878b1b710a
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-08-26 09:51:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
+    int missMul(vector<int> &nums, int k){
+        while()
+    }
     int missingMultiple(vector<int>& nums, int k) {
         int n = nums.size();
-        int ans = 0;
-        for(int i=k; i<=1000; i += k){
-            bool flag = false;
-            for(int j=0; j<n; j++){
-                if(i == nums[j]){
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag == false){
-                ans = i;
-                break;
-            }
-        }
+        int j=1;
+        
 
-        return ans;
+        int MissMul(nums, k);
     }
 };

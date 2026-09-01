cabea405192262996b12f1501c6a43cd073ef585
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-09-01 11:37:18 +0530

    Time: 1 ms (42.24%), Space: 10 MB (59.66%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int index, vector<int> &nums, vector<int> &temp){
+        if(index == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        temp.push_back(nums[index]);
+        solve(index+1, nums, temp);
+
+        temp.pop_back();
+        solve(index+1, nums, temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+
+        solve(0, nums, temp);
+        return ans;
+    }
+};

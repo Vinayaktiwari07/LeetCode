9b971207bb3e7b52e334229a747b4e249d228db1
Vinayaktiwari07 <vinayaktiwari00326@gmail.com>
2026-09-08 10:38:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s = to_string(n);
-        int size = s.size();
-        int count = 0;
-
-         // 1 comma (1000 → 999999)
-        if (n >= 1000) {
-            long long upper = min((long long)n, 999999LL);
-            count += (upper - 1000 + 1) * 1;
+        // int count = 0;
+        // int rdm = n;
+        // while(rdm>0){
+        //     rdm = rdm/10;
+        //     count++;
+        // }
+        int ans = 0;
+        if(n>=1000){
+            ans = (n-1000)+1;
         }
 
-        // 2 commas (1000000 → 999999999)
-        if (n >= 1000000) {
-            long long upper = min((long long)n, 999999999LL);
-            count += (upper - 1000000 + 1) * 2;
-        }
-
-        // 3 commas (1000000000 → 999999999999)
-        if (n >= 1000000000) {
-            long long upper = min((long long)n, 999999999999LL);
-            count += (upper - 1000000000LL + 1) * 3;
-        }
-
-        // 4 commas (>= 10^12)
-        if (n >= 1000000000000LL) {
-            count += ((long long)n - 1000000000000LL + 1) * 4;
-        }
-
-        return (int)count;
+        return ans;
 
     }
 };
